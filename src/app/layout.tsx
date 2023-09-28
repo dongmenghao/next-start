47ef9e9ca76f1a09780d91338128e1568b6f1ea1
dongmenghao <dongmh105@qq.com>
2023-09-28 11:59:52 +0800

    add tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import './globals.css'
+import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from '../lib/AntdRegistry';
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <StyledComponentsRegistry>
           <ConfigProvider theme={antdTheme}>{children}</ConfigProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import { Button } from 'antd';
 
 function Home() {
   return (
-    <div>
-      <div>Hello, next.js</div>
-      <Button type="primary">Button</Button>
+    <div className="text-center my-20 p-10 dark:bg-gray-700">
+      <div className="text-3xl font-medium">Hello, next.js</div>
+      <Button className="mt-4" type="primary">
+        Button
+      </Button>
     </div>
   );
 }
