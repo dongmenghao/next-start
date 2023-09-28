ad1070d7ae479bee272048986ba75968bf2e8290
dongmenghao <dongmh105@qq.com>
2023-09-28 11:03:44 +0800

    add antd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 // import './globals.css'
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import StyledComponentsRegistry from '../lib/AntdRegistry';
+import { ConfigProvider } from 'antd';
+import { antdTheme } from '@/theme/themeConfig';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +15,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          <ConfigProvider theme={antdTheme}>{children}</ConfigProvider>
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from 'antd';
+
 function Home() {
   return (
     <div>
       <div>Hello, next.js</div>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
